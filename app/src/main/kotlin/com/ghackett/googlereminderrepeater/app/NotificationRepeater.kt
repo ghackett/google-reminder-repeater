d536cf550f4ec4031e43636424b6cd3b11b754cc
Geoff Hackett <ghackett@gmail.com>
2022-11-10 10:41:31 -0500

    google package name

--- app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/NotificationRepeater.kt
+++ app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/NotificationRepeater.kt
@@ -4,8 +4,11 @@ import android.app.NotificationManager
 import android.service.notification.StatusBarNotification
 import javax.inject.Inject
 
+private const val GOOGLE_PACKAGE = "com.google.android.googlequicksearchbox"
 class NotificationRepeater @Inject constructor(notificationManager: NotificationManager) {
   fun onNotificationPosted(sbn: StatusBarNotification) {
+    if (sbn.packageName != GOOGLE_PACKAGE) return
+
 
   }
 }
