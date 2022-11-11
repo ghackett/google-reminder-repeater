b2ab867d9d6e3667d41415c6db91d5bef8ccb5fb
Geoff Hackett <ghackett@gmail.com>
2022-11-11 11:05:39 -0500

    Notifier refactor - make lambda function inline & pass in resources

--- app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/notifications/Notifier.kt
+++ app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/notifications/Notifier.kt
@@ -1,20 +1,34 @@
 package com.ghackett.googlereminderrepeater.app.notifications
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.res.Resources
 import androidx.core.app.NotificationCompat
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class Notifier @Inject constructor(
   private val notificationManager: NotificationManager,
-  @ApplicationContext private val appContext: Context,
+  @ApplicationContext val appContext: Context,
 ) {
-  fun notify(channel: NotificationChannelInfo, id: Int, action: NotificationCompat.Builder.() -> Unit) {
-    notificationManager.prep(channel)
-    notificationManager.notify(id, NotificationCompat.Builder(appContext, channel.id).apply(action).build())
+  fun notify(channel: NotificationChannelInfo, id: Int, notification: Notification) {
+    notificationManager.run {
+      prep(channel)
+      notify(id, notification)
+    }
   }
+}
+
+inline fun Notifier.notify(channel: NotificationChannelInfo, id: Int, action: NotificationCompat.Builder.(Resources) -> Unit) {
+  notify(
+    channel = channel,
+    id = id,
+    notification = NotificationCompat.Builder(appContext, channel.id)
+      .apply { action(appContext.resources) }
+      .build()
+  )
 }
 
 private fun NotificationManager.prep(channel: NotificationChannelInfo) {
