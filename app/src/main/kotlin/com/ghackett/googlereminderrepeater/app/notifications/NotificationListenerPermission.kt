38996fdaec7536fd2cca427fdaab4c15d5d6999b
Geoff Hackett <ghackett@gmail.com>
2022-11-13 12:22:28 -0500

    fix lint

--- app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/notifications/NotificationListenerPermission.kt
+++ app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/notifications/NotificationListenerPermission.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
@@ -18,13 +19,14 @@ enum class NotificationListenerPermission { GRANTED, NOT_GRANTED, UNKNOWN }
 
 @Composable fun notificationListenerPermissionState(): State<NotificationListenerPermission> {
   val context = LocalContext.current
-  return LocalLifecycleOwner.current.lifecycle
-    .eventFlow()
-    .filter { it == Lifecycle.Event.ON_RESUME }
-    .map { context.isNotificationListenerAccessGranted() }
-    .collectAsState(initial = context.isNotificationListenerAccessGranted())
+  val lifecycle = LocalLifecycleOwner.current.lifecycle
+  val flow = remember {
+    lifecycle.eventFlow()
+      .filter { it == Lifecycle.Event.ON_RESUME }
+      .map { context.isNotificationListenerAccessGranted() }
+  }
+  return flow.collectAsState(initial = context.isNotificationListenerAccessGranted())
 }
-
 
 private fun Context.isNotificationListenerAccessGranted(): NotificationListenerPermission {
   if (Build.VERSION.SDK_INT < 27) return NotificationListenerPermission.UNKNOWN
