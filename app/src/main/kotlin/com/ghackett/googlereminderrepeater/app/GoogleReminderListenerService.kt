9c563047bd4d8a6ee24cee25e74ff269f250be25
Geoff Hackett <ghackett@gmail.com>
2022-11-12 21:24:13 -0500

    refactor some logic + packages

--- app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/GoogleReminderListenerService.kt
+++ app/src/main/kotlin/com/ghackett/googlereminderrepeater/app/GoogleReminderListenerService.kt
@@ -2,13 +2,13 @@ package com.ghackett.googlereminderrepeater.app
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
-import com.ghackett.googlereminderrepeater.app.notifications.NotificationRepeater
+import com.ghackett.googlereminderrepeater.app.googlenotifications.GoogleNotificationRepeater
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint class GoogleReminderListenerService : NotificationListenerService() {
 
-  @Inject lateinit var repeater: NotificationRepeater
+  @Inject lateinit var repeater: GoogleNotificationRepeater
 
   override fun onNotificationPosted(sbn: StatusBarNotification?) {
     super.onNotificationPosted(sbn)
