ba0981ba2c175b1ae1786c7e9f628c590b7f14f2
Geoff Hackett <ghackett@gmail.com>
2022-12-11 19:41:13 -0500

    Delete un-needed gradle, but define an android lib plugin (#12)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,25 +4,9 @@ plugins {
 
 gradlePlugin {
   plugins {
-    create("ConfigureSitePlugin") {
-      id = "config-site"
-      implementationClass = "plugins.ConfigSitePlugin"
-    }
     create("ConfigureJvmPlugin") {
       id = "config-jvm"
       implementationClass = "plugins.ConfigureJvmPlugin"
-    }
-    create("ConfigureAndroidPlugin") {
-      id = "config-android"
-      implementationClass = "plugins.ConfigureAndroidPlugin"
-    }
-    create("ConfigureAndroidDeployablePlugin") {
-      id = "config-android-deploy"
-      implementationClass = "plugins.ConfigureAndroidDeployablePlugin"
-    }
-    create("ConfigureJvmDeployablePlugin") {
-      id = "config-jvm-deploy"
-      implementationClass = "plugins.ConfigureJvmDeployablePlugin"
     }
     create("ConfigureAndroidComposePlugin") {
       id = "config-compose"
@@ -31,6 +15,10 @@ gradlePlugin {
     create("ConfigureAndroidAppPlugin") {
       id = "config-android-app"
       implementationClass = "plugins.ConfigureAndroidAppPlugin"
+    }
+    create("ConfigureAndroidLibPlugin") {
+      id = "config-android-lib"
+      implementationClass = "plugins.ConfigureAndroidLibPlugin"
     }
   }
 }
